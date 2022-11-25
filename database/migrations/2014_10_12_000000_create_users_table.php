666016eab108d4052ef1dde2d15a13534b97a208
HarisMI40 <harismaulanaikhsan40@gmail.com>
2022-11-25 10:43:33 +0700

    + laravel breeze

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nama_lengkap');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('username');
             $table->string('password');
+            $table->string('no_hp');
+            $table->enum('is_active', [true, false]);
             $table->rememberToken();
             $table->timestamps();
         });
